a526a729d763895f6a794d0314c722e5d0688454
Gleisson Braga <bragagleisson@gmail.com>
2024-04-14 10:11:28 -0300

    feat: criando a estilização da pagina main

--- public/index.php
+++ public/index.php
@@ -1,4 +1,13 @@
 <?php
 
 require_once(dirname(__FILE__, 2) . '/src/config/config.php');
-require_once(CONTROLLER_PATH . '/login.php');
+// require_once(CONTROLLER_PATH . '/login.php');
+// require_once(CONTROLLER_PATH . '/day_records.php');
+
+$uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+
+if($uri === '/' or $uri === '' or $uri === '/index.php'){
+    $uri = '/login.php';
+}
+
+require_once(CONTROLLER_PATH . "/{$uri}");
---
+++ src/views/template/footer.php
@@ -0,0 +1,7 @@
+<footer class="footer">
+    <span>Copyright COD3R</span>
+
+</footer>
+
+</body>
+</html>
