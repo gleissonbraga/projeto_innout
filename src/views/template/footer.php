547280459b728b74d3fd2b0caddb2cb758f33581
Gleisson Braga <bragagleisson@gmail.com>
2024-04-14 20:34:22 -0300

    feat: adicionando estilização ao projeto

--- src/views/template/footer.php
+++ src/views/template/footer.php
@@ -2,6 +2,6 @@
     <span>Copyright COD3R</span>
 
 </footer>
-
+<script src="assets/js/app.js"></script>
 </body>
 </html>
--- src/views/template/header.php
+++ src/views/template/header.php
@@ -7,12 +7,13 @@
     <link rel="stylesheet" href="assets/css/comum.css">
     <link rel="stylesheet" href="assets/css/icofont.min.css">
     <link rel="stylesheet" href="assets/css/bootstrap.min.css">
+
     <title>In N' Out</title>
 </head>
 <body>
     <header class="header">
         <div class="logo">
-            <i class="icofont-travelling mr-2"></i>
+            <i class="icofont-travelling mr-2 "></i>
 
             <span class="font-weight-light">In</span>
             <span class="font-weight-bold mx-2">N'</span>
@@ -23,5 +24,26 @@
         <div class="menu-toggle mx-3">
             <i class="icofont-navigation-menu"></i>
         </div>
+        
         <div class="spacer"></div>
+        
+        
+        <div class="dropdown">
+            <div class="dropdown-button">
+                <span class="ml-2">
+                    Usuário Mock
+                    <i class="icofont-simple-down mx-2"></i>
+                </span>
+                <div class="dropdown-content">
+                    <ul class="nav-list">
+                        <li class="nav-item">
+                            <a href="logout.php">
+                                <i class="icofont-logout mr-1"></i>
+                                Sair
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     </header>
