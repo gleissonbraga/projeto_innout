1b402b52356bde149ac11908d86ce50e75b540a2
Gleisson Braga <bragagleisson@gmail.com>
2024-04-23 07:45:49 -0300

    feat: adicionando funcionalidade de horas trabalhadas

--- src/controllers/teste.php
+++ src/controllers/teste.php
@@ -7,4 +7,13 @@ loadModel('WorkingHours');
 
 $wh = WorkingHours::loadFromUserAndDate(1, date('Y-m-d'));
 
+$workedInterval = $wh->getWorkedInterval()->format('%H:%I:%S');
+print_r($workedInterval);
 echo '<br>';
+
+$lunchInterval = $wh->getLunchInterval()->format('%H:%I:%S');
+print_r($lunchInterval);
+echo '<br>';
+
+
+print_r($wh->getExitTime());
